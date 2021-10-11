383e46e1fd909fe5cebf607317f5907142b2affc
sagar <7208sk@gmail.com>
2021-10-11 17:04:00 +0530

    ReverseNumber

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Day6_Logical_Programs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            static void Main(string[] args)
+            {
+                int Number, Reverse = 0, Rem;
+                Console.WriteLine("Enter a number to reverse");
+                Number = int.Parse(Console.ReadLine());
+
+                while (Number != 0)
+                {
+                    Rem = Number % 10;
+                    Reverse = Reverse * 10 + Rem;
+                    Number /= 10;
+                }
+                Console.WriteLine("Reversed Number: {0}", Reverse);
+                Console.WriteLine();
+            }
         }
     }
 }
