b70b3806a25d39dfc3200447cbc506b3a6bb7959
sagar <7208sk@gmail.com>
2021-10-11 16:34:27 +0530

    perfectNumber

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace Day6_Logical_Programs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            int n, i, sum;
+
+
+            Console.Write("Enter the  number for checking perfect or not  : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            sum = 0;
+            Console.Write("The positive divisor  : ");
+            for (i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                    Console.Write("{0}  ", i);
+                }
+            }
+            Console.Write("\nThe sum of the divisor is : {0}", sum);
+            if (sum == n)
+                Console.Write("\nthe number is perfect.");
+            else
+                Console.Write("\nthe number is not perfect.");
+            Console.Write("\n");
         }
     }
 }
